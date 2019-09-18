9b1bacc2a5f203436a3a5b1e46be72540ec5b418
oisindillon <o.dillon@lancaster.ac.uk>
2019-09-18 20:12:07 +0200

    Additional creation of classes and interfaces
    Co-Authored-By: Beata Haracewiat <haracewiat@users.noreply.github.com>

---
+++ Identifier.java
@@ -0,0 +1,5 @@
+public class Identifier extends Identifiable{
+
+    
+
+}
---
+++ Problem1.java
@@ -0,0 +1,16 @@
+public class Problem1{
+    public static void main(String args[]){
+        System.out.println("Welcome, follow the following rules to find the difference, intersection, union and symmetric difference between two sets.");
+        boolean willThisEnd = false;
+        while(willThisEnd == false){        //while loop so the program will continually run even when problem gets finished the first time
+            System.out.print("Enter your first set: ");
+            System.out.println("");
+            Set set1 = new Set();
+            set1.errorCheck();
+
+            System.out.print("Enter your second set: ");
+            System.out.println("");
+
+        }
+    }
+}
