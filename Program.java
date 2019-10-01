841767f277d9461e03aeda58fe6c0fff778e5a85
Beata Haracewiat <b.haracewiat@gmail.com>
2019-10-01 21:44:52 +0200

    Set Program.java to default

--- Program.java
+++ Program.java
@@ -23,46 +23,22 @@ public class Program{
     
     
     boolean getSet (Scanner input, String question, Set set) {
-        String setToValidate;
-
-        System.out.printf(question);
-        do {
-            input.nextLine();
-            if (input.hasNext()) {
-                setToValidate = input.next();
-                System.out.printf("Set: " + setToValidate);
-            } else {
-                System.out.printf("No input given.\n");
-            }
-        } while (! input.hasNext());
+        String setToValidate = "";
         
-
-        /*
+       
         do {
-            
-
-            if (input.hasNext()) {
-                
-            } else {
-                System.out.printf("has no next\n");
+            System.out.printf(question);
+            if (! input.hasNextLine()) {
+                System.out.printf("has no next line\n");
                 return false;
-            }
-        } while (! validSet(input, set));         
-
-        */
+            } 
+        } while (! validSet(input, setToValidate));         
+     
 
         return true;
     }
-
     
-    static String function(Scanner in) {
-        
-        System.out.print("Input: ");
-        String setToValidate = in.next();
-        return setToValidate;
-    }
-    
-    boolean validSet(Scanner input, Set set) {
+    boolean validSet(Scanner input, String setToValidate) {
         boolean valid = true;
         //call function to check errors
         if(valid) {
